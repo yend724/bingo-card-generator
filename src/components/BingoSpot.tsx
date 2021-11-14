3d1da88f6677fd3368d6bc63d55e9d7192f74b0a
yend724 <yend9524@gmail.com>
2021-11-14 20:14:59 +0900

    Update BingoSpot

--- src/components/BingoSpot.tsx
+++ src/components/BingoSpot.tsx
@@ -5,17 +5,21 @@ type Props = {
 };
 const BingoSpot: React.FC<Props> = ({ number }) => {
 	const [open, setOpen] = useState(false);
+	const onClickOpen = () => {
+		setOpen(true);
+	};
 
 	return (
-		<div className="w-12 h-12 mt-2 ml-2 bg-white" onClick={() => setOpen(true)}>
-			<div
-				className="flex items-center justify-center w-full h-full"
-				style={{ perspective: "500px" }}
-			>
+		<div
+			className="w-12 h-12 mt-2 ml-2 bg-white cursor-pointer group"
+			onClick={onClickOpen}
+		>
+			<div className="flex items-center justify-center w-full h-full perspective-500">
 				<span className="inline-block bg-gray-300 rounded-t-xl">
 					<span
-						className="inline-block w-8 h-8 bg-white rounded-t-xl text-center origin-bottom"
-						style={{ transform: `${open ? "rotateX(45deg)" : "none"}` }}
+						className={`inline-block w-8 h-8 bg-white rounded-t-xl text-center transform-gpu translate-y-px origin-bottom ${
+							open ? "rotate-x-60" : "group-hover:opacity-50"
+						}`}
 					>
 						{number === 0 ? "Free" : number}
 					</span>
